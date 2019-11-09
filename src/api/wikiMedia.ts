209fd98ff21d0ca7321c4bd0dd8b50be487b71d2
davidkwan95 <davidkwan95@gmail.com>
2019-11-09 13:11:12 +0700

    create wikiMedia api client

---
+++ src/api/wikiMedia.ts
@@ -0,0 +1,61 @@
+/**
+ * Client class to help access WikiMedia API
+ * Using the English Wikipedia API endpoint by default
+ */
+
+import superagent from "superagent";
+
+interface IWikiMediaAPISearchItem {
+  ns: number;
+  title: string;
+  pageid: number;
+  size: number;
+  wordcount: number;
+  snippet: string;
+}
+
+interface IWikiMediaAPISearchBodyResponse {
+  continue: {
+    sroffset: number;
+  };
+  query: {
+    searchinfo: {
+      totalhits: number;
+      suggestion: string;
+      suggestionsnippet: string;
+    };
+    search: IWikiMediaAPISearchItem[];
+  };
+}
+
+export default class WikiMediaAPI {
+  private endpoint: string;
+
+  constructor(endpoint: string = "https://en.wikipedia.org/w/api.php") {
+    this.endpoint = endpoint;
+  }
+
+  public setEndpoint(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  public getEndpoint(): string {
+    return this.endpoint;
+  }
+
+  public async search(
+    searchPhrase: string,
+    sroffset: number = 0
+  ): Promise<IWikiMediaAPISearchBodyResponse> {
+    const endpoint = this.endpoint;
+    const res = await superagent.get(endpoint).query({
+      action: "query",
+      list: "search",
+      srsearch: searchPhrase,
+      sroffset,
+      format: "json"
+    });
+
+    return res.body;
+  }
+}
